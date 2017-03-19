1ed31c267ee624547c731fe1d3af99adee9e4dfe
Fredrik A. Krisiansen <fredrikaxk@gmail.com>
2017-03-19 21:23:31 +0100

    Initial Commit

---
+++ FredrikScript/Ast.fs
@@ -0,0 +1,86 @@
+﻿module Ast
+
+type Name = Name of string
+type TypeName = 
+    TypeName of string * Rank list option
+and Rank = Pointer | Array
+
+type AccessModifier = Internal|Private|Protected|Public
+type StorageClass = 
+    |Instance
+    |Static
+    |Extern of string
+
+type CompilationUnit = CompilationUnit of UsingDirective list * TopLevelDeclaration list
+and UsingDirective = UsingDirective of string
+and TopLevelDeclaration =
+    | Type of Type
+    | Namespace of Name * TopLevelDeclaration list
+and Type =
+    | Enum of AccessModifier * Name * EnumValue list
+    | Class of AccessModifier * Name * TypeMember list
+    | Struct of AccessModifier * Name * TypeMember list
+    | Interface of AccessModifier * Name * InterfaceMember list
+and EnumValue = EnumValue of Name * int option
+and TypeMember =
+    | Constructor of AccessModifier * Parameter list * Statement list
+    | Field of AccessModifier * StorageClass * TypeName * Name
+    | Method of AccessModifier * StorageClass * TypeName * Name * Parameter list * Statement list
+    | MethodDefinition of AccessModifier * StorageClass * TypeName * Name * Parameter list
+and InterfaceMember =
+    | Method of TypeName * Name * Parameter list
+and Parameter =
+    | Parameter of TypeName * Name
+and Statement =
+    | Return of Expression option
+and Expression =
+    | Integer of int32
+    | Long of int64
+    | Float of float32
+    | Double of double
+    | Boolean of bool
+    | Char of char
+    | String of string
+    | Symbol of string
+    | Add of Expression * Expression
+    | Subtract of Expression * Expression
+    | Multiply of Expression * Expression
+    | Divide of Expression * Expression
+    | Modulus of Expression * Expression
+    | BitshiftLeft of Expression * Expression
+    | BitshiftRight of Expression * Expression
+    | Ternary of Expression * Expression * Expression
+    | UnaryPlus of Expression
+    | UnaryMinus of Expression
+    | Not of Expression
+    | BitwiseNot of Expression
+    | PrefixIncrement of Expression
+    | PrefixDecrement of Expression
+    | SuffixIncrement of Expression
+    | PostfixIncrement of Expression
+    | PostfixDecrement of Expression
+    | And of Expression * Expression
+    | Or of Expression * Expression
+    | BitwiseAnd of Expression * Expression
+    | BitwiseOr of Expression * Expression
+    | BitwiseXor of Expression * Expression
+    | Equals of Expression * Expression
+    | NotEqual of Expression * Expression
+    | GreaterThan of Expression * Expression
+    | GreaterThanEquals of Expression * Expression
+    | LowerThan of Expression * Expression
+    | LowerThanEquals of Expression * Expression
+    | Assignment of Expression * Expression
+    | AddAssignment of Expression * Expression
+    | SubtractAssignment of Expression * Expression
+    | MultiplyAssignment of Expression * Expression
+    | DivideAssignment of Expression * Expression
+    | ModulusAssignment of Expression * Expression
+    | BitshiftLeftAssignment of Expression * Expression
+    | BitshiftRightAssignment of Expression * Expression
+    | BitwiseAndAssignment of Expression * Expression
+    | BitwiseXorAssignment of Expression * Expression
+    | BitwiseOrAssignment of Expression * Expression
+    | Member of Expression * Expression
+    | Index of Expression * Expression
+    | Call of string * Expression list
---
+++ FredrikScript/Program.fs
@@ -0,0 +1,28 @@
+﻿open Parser
+open FParsec
+
+let printSuccess x =
+    printfn "%A\n\n" x
+let printFailure x =
+    printfn "%A\n\n" x
+
+let test p input = 
+    match run p input with
+    | Success (r, _, _) -> printSuccess r
+    | Failure (err, _, _) -> printFailure err
+
+[<EntryPoint>]
+let main argv = 
+    test pCodeBlock "{return x == z.i.ToString() ? y : i[0].i.ToString(); return;}"
+//    test pCompilationUnit "using System.Data;using System.Net;class FFF{}namespace System.IO { class ff{ int[] PrintData(object o) {return x(0, \"Hello World!\");}int PrintData2(object o) {return;} int y;} namespace Async{enum DaFgh { SOME_ID = 0, SOME_DATA=1, SOMEOTHER } }}"
+//    test pCompilationUnit "using System.Data;using System.Net;class FFF{}namespace System.IO { class ff{ int[] PrintData(object o) {return x(0, \"Hello World!\");}int PrintData2(object o) {return;} int y;} namespace Async{enum DaFgh { SOME_ID = 0, SOME_DATA=1, SOMEOTHER } }}"
+//    test pType "public interface IDisposable{void Dispose();} "
+//    test pType "public enum TEst { DATAID=0,DATAID2,DATAID3=4 }"
+//    test pType "public struct TEst { private constructor(int x) {} int x  ; void* y()  ;  extern \"ff\" void z() {return x.x.x.x.SomeMethod() + x + 100.0f;}}"
+    //test pMember "constructor (int x, int y) {return 0; return                ;}"
+    //test pMember "static int x() { return 0 ;}"
+    //test pMember "static int x();"
+    //test pMember "int x;"
+
+    printfn "%A" argv
+    0 // return an integer exit code
